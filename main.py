8aaae547f3d0a11d2101ea5305f9c9c31223bfd0
5mbl <nyrox.plg@gmail.com>
2025-07-25 01:54:08 +0200

    add cors support

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow requests from Next.js app
+    allow_origins=["*"],  # Allow requests from Next.js app
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
